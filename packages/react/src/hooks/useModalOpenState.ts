62e49f15d6cd34f87fc10e50568654880ef02829
polarb-raf <doom0728@gmail.com>
2023-10-20 13:00:31 +0900

    add useModalOpenState

---
+++ packages/react/src/hooks/useModalOpenState.ts
@@ -0,0 +1,25 @@
+import { useState, useEffect, Dispatch, SetStateAction } from 'react';
+
+interface ModalOpenStateHookParams {
+  parentElId: string; // modal을 담고 있는 부모 요소의 element Id. unique해야 오동작을 방지할 수 있으며 css selector의 형태로 전송하자. ex) #testModal
+  defaultState?: boolean;
+}
+
+const useModalOpenState = ({ parentElId, defaultState = false }: ModalOpenStateHookParams): [boolean, Dispatch<SetStateAction<boolean>>] => {
+  const [isOpened, setOpenedState] = useState<boolean>(defaultState);
+  useEffect(() => {
+    const checkClickOutside = (e: MouseEvent) => {
+      const target = e.target as Element;
+      if (target.closest(`${parentElId}`)) return;
+      setOpenedState(false);
+    };
+    document.addEventListener('mousedown', checkClickOutside);
+    return () => {
+      document.removeEventListener('mousedown', checkClickOutside);
+    };
+  }, [setOpenedState, parentElId]);
+
+  return [isOpened, setOpenedState];
+};
+
+export default useModalOpenState;
