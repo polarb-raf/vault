8b73119dbeee6149cbe379f1ce5e492bf1797984
Woongki Rafael Kim <doom0728@gmail.com>
2023-10-24 18:59:56 +0900

    design system workspace init

---
+++ packages/design-system/README.md
@@ -0,0 +1,8 @@
+# @vault/design-system
+
+---
+회사 내 디자인 시스템 만드는 일을 리딩하게 되었는데, 연구 및 라이브러리 테스트 겸 만들어보는 package.
+
+## 기술 스택
+- react
+- tailwindcss
---
+++ packages/design-system/vite.config.ts
@@ -0,0 +1,21 @@
+import { resolve } from 'node:path';
+
+import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  resolve: {
+    alias: {
+      '@': resolve(__dirname, './src'),
+    },
+  },
+  build: {
+    lib: {
+      entry: resolve(__dirname, './src/index.ts'),
+      fileName: 'components',
+      formats: ['es'],
+    },
+  },
+});
