2830107e28fe8316074edce49e33a236627c6316
Woongki Rafael Kim <doom0728@gmail.com>
2023-10-24 15:22:37 +0900

    fix config

--- packages/react/src/hooks/useDebounce.ts
+++ packages/react/src/hooks/useDebounce.ts
@@ -2,6 +2,7 @@ import { useEffect, useMemo, useState } from 'react';
 
 import { debounce } from '@/utils/debounce';
 
+// React18 이후 버전을 사용한다면 useDeferredValue를 사용하는 것이 나을 수 있다.
 function useDebounce<T>(value: T, delay: number): T {
   const [debouncedState, setState] = useState(value);
 
