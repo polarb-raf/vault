48863b97e2a47381f9844b2e237951eb69c856c8
Woongki Rafael Kim <doom0728@gmail.com>
2023-11-07 16:13:41 +0900

    [WIP] preset setting

--- packages/design-system/preset.js
+++ packages/design-system/preset.js
@@ -1,10 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 
-import preset from '@vault/design-system/preset';
 export default {
-  presets: [preset],
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
+    colors: {
+      blue: {
+        500: '#000',
+      },
+      primary: '#992233',
+    },
     extend: {},
   },
   plugins: [],
--- packages/design-system/tailwind.config.js
+++ packages/design-system/tailwind.config.js
@@ -1,14 +1,7 @@
 /** @type {import('tailwindcss').Config} */
+
+import preset from './preset';
+
 export default {
-  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
-  theme: {
-    colors: {
-      blue: {
-        500: '#000',
-      },
-      primary: '#992233',
-    },
-    extend: {},
-  },
-  plugins: [],
+  ...preset,
 };
